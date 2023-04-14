b450ec92063a53b53d0e05a67881ddbbf9037b1d
mhkarimi78 <mahsa_karim16@yahoo.com>
2023-04-14 11:11:59 +0330

    added frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 5001;
-const Moralis = require("cors");
+const Moralis = require("moralis").default;
+const cors = require("cors");
 
 require("dotenv").config({
   path: ".env",
@@ -16,7 +17,7 @@ app.get("/getethprice", async (req, res) => {
   try {
     const response = await Moralis.EvmApi.token.getTokenPrice({
       address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-      chain: chainId,
+      chain: "0x1",
     });
     return res.status(200).json(response);
   } catch (e) {
